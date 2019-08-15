ca9854afed3c4bee2a09dcdbce6150dfb18954ee
qdlaty <lukasz.orzel@gmail.com>
2019-08-15 15:16:09 +0200

    1

---
+++ src/Controller/AppController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/", name="app")
+     * @param int $a
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
+    public function index($a = 1)
+    {
+
+        if($a) {
+            print_r(1);
+        }
+
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+}
